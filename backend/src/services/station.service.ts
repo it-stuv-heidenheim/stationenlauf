45e54886e482783ba2e6abd0ccae7e9a2ef40ff9
JulianBM23 <107028217+JulianBM23@users.noreply.github.com>
2025-09-30 16:32:32 +0200

    fix(station-api): Fixed task codes getting shown when listing all stations

--- backend/src/services/station.service.ts
+++ backend/src/services/station.service.ts
@@ -1,17 +1,23 @@
-import { prisma } from "../db/prisma.js"
+import {prisma} from "../db/prisma.js"
 
 export async function listStations() {
   const items = await prisma.station.findMany({
-      orderBy: { name: "asc" },
-      include: { tasks: true }
+    orderBy: {name: "asc"},
+    include: {tasks: true},
+  })
+  items.forEach(el => {
+    el.tasks.forEach(e => {
+      // @ts-ignore
+      delete e.code
     })
-  return { data: items }
+  })
+  return {data: items}
 }
 
 export async function getStationById(id: string) {
   const station = await prisma.station.findUnique({
-    where: { id },
-    include: { tasks: true }
+    where: {id},
+    include: {tasks: true}
   })
   if (!station) {
     const e = new Error("Station not found")
