4a0bbce35207c005697dcdceb9689f79792ca11a
JulianBM23 <107028217+JulianBM23@users.noreply.github.com>
2025-09-30 23:47:10 +0200

    fix(cors): Explicitly allowing two known origin domains, but keeping wildcard

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({origin: [
-    '*'
+    '*',
+    'http://localhost:5173',
+    'https://stationenlauf.stuv-heidenheim.de'
   ]
 }));
 app.use(express.json());
